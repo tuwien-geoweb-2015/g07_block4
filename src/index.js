0626b5ab74b4897104d06033076f7e4e809ca3d4
linkras <lindiella.k@gmail.com>
2015-11-11 14:47:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ require('../node_modules/openlayers/dist/ol.css');
 require('./index.css');
 
 var ol = require('openlayers');
+var send = require('./feedback_send');
+
+var url = 'http://student.ifip.tuwien.ac.at/geoserver/wfs';
+var layer = 'feedback';
+var prefix = 'g07_2015';
+var featureNS = 'http://g07/2015';
+var form = document.getElementById('feedback');
+
+var feedbackPoints = new ol.source.Vector({
+  features: new ol.Collection(),
+});
 
 var map = new ol.Map({
   controls: ol.control.defaults({attributionOptions: {collapsible: false}}),
@@ -10,9 +21,43 @@ var map = new ol.Map({
     new ol.layer.Tile({
       source: new ol.source.OSM()
     })
+    new ol.layer.Tile({
+      source: new ol.source.TileWMS({
+        url: url,
+        params: {LAYERS: prefix + ':' + layer}
+      })
+    }),
+    new ol.layer.Vector({
+      source: feedbackPoints
+    })
   ],
   view: new ol.View({
     center: ol.proj.fromLonLat([16.373064, 48.20833]),
     zoom: 13
   })
+});
+
+var feature = new ol.Feature();
+feature.setGeometryName('geom');
+feature.setGeometry(new ol.geom.Point(map.getView().getCenter()));
+feedbackPoints.addFeature(feature);
+var modify = new ol.interaction.Modify({
+  features: feedbackPoints.getFeaturesCollection()
+});
+map.addInteraction(modify);
+
+var geolocation = new ol.Geolocation({
+  projection: map.getView().getProjection(),
+  tracking: true
+});
+geolocation.once('change:position', function(evt) {
+  feature.getGeometry().setCoordinates(geolocation.getPosition());
+  map.getView().setCenter(geolocation.getPosition());
+});
+
+send(form, feature, url, {
+  featureType: layer,
+  featurePrefix: prefix,
+  featureNS: featureNS,
+  srsName: map.getView().getProjection().getCode()
 });
